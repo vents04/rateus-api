893e85e27242a3a68563304d00fa01678bae6953
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-04-02 23:19:16 +0300

    Business model init

---
+++ db/models/business.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const businessSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    emailOrPhone:{
+        type: String,
+        required: true,
+        minLength: 1,
+        trim: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true,
+        minlength: 8,
+    },
+    color:{
+        type: String,
+        required: true
+    },
+    accountCreation:{
+        type: Date,
+        default: Date.now
+    }
+})
+
+const Business = mongoose.model("Business", businessSchema);
+module.exports = {Business}
