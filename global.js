ceeaf1c9211333add819e91c4b73473a6730cc5c
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-04-02 22:03:25 +0300

    server.js init

---
+++ global.js
@@ -0,0 +1,6 @@
+module.exports = {
+    ROOT_URL: 'http://localhost:8080',
+    ROOT_URL_FRONTEND: 'http://localhost:4200',
+    JWT_SECRET: 'IMjbVViINeVwmvmj2BWg',
+    SENDGRID_API_KEY: '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'
+}
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+//const mongoDb = require('./db/mongoose');
+const { ROOT_URL, ROOT_URL_FRONTEND, ROOT_URL_KEEPER } = require('./global');
+
+const allowedOrigins = [ROOT_URL, ROOT_URL_FRONTEND, ROOT_URL_KEEPER];
+
+app.use(cors({
+    origin: function(origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            let msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(msg, false);
+        }
+        return callback(null, true);
+    }
+}));
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,auth-token');
+    res.setHeader('Access-Control-Expose-Headers', '*');
+    res.setHeader('X-Powered-By', 'Hello, world!');
+    next();
+});
+
+app.use(bodyParser.json({ limit: '100MB' })).use(express.urlencoded({ extended: true }));
+
+//const {
+//} = require('./routes/all-routes.import');
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log("Server listening on port 8080");
+});
