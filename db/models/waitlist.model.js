8c5978c2b4aaeb75621d0543b6a38f077af3cc3a
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-04-02 22:35:04 +0300

    init

---
+++ db/models/waitlist.model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const waitlistSchema = new mongoose.Schema({
+    address: {
+      type: String,
+      required: true,
+    },
+    emailOrPhone: {
+      type: String,
+      required: true,
+    }
+})
+
+const Waitlist = mongoose.model("Waitlist", waitlistSchema);
+module.exports = {Waitlist}
