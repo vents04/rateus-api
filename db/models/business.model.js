049e28016343535194811dc74c087cb526bb1459
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-04-03 01:23:05 +0300

    Add unique property to uId

--- db/models/business.model.js
+++ db/models/business.model.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const businessSchema = new mongoose.Schema({
     uId: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     name:{
         type: String,
