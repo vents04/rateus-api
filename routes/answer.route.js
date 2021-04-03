4ff8cd9f98ddefb5a61ed83f8797e82e0d88ea5c
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-04-04 00:01:10 +0300

    Add create method

--- routes/answer.route.js
+++ routes/answer.route.js
@@ -2,17 +2,24 @@ const express = require('express');
 const router = express.Router();
 
 const ErrorHandler = new(require('../services/error-handling.service').ErrorHandler)();
-const WaitlistService = new(require('../services/waitlist.service').WaitlistService)();
+const AnswerService = new(require('../services/answer.service').AnswerService)();
+const QuestionnaireService = new(require('../services/questionnaire.service').QuestionnaireService)();
 
-const { createWaitlistValidation } = require('../validation/validation');
+const { createAnswerValidation } = require('../validation/validation');
 
 router.post('/', (req, res) => {
-    const { error } = createWaitlistValidation(req.body);
+    const { error } = createAnswerValidation(req.body);
     if (error) return ErrorHandler.returnError({ 'errorCode': 400, 'errorMessage': error.details[0].message }, res);
 
-    WaitlistService.createWaitlist(req.body).then((waitlist) => {
+    QuestionnaireService.getQuestionnaire({ _id: req.body.questionnaireId }).then((questionnaire) => {
+        if(questionnaire == null) {
+            return ErrorHandler.returnError({ 'errorCode': 400, 'errorMessage': "No such questionnaire." }, res);
+        }
+    })
+
+    AnswerService.createAnswer(req.body).then((answer) => {
         res.status(200).send({
-            waitlist: waitlist
+            answer: answer
         })
     }).catch((err) => {
         return ErrorHandler.returnError(err, res);
