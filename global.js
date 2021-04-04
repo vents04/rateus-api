55b00d10b34ee4922bdf6192875ed229f29e36fc
vents04 <vencidim04@gmail.com>
2021-04-04 19:08:59 +0300

    global root url fixed

--- global.js
+++ global.js
@@ -1,5 +1,5 @@
 module.exports = {
-    ROOT_URL: 'http://localhost:8080',
+    ROOT_URL: 'http://localhost:8081',
     ROOT_URL_FRONTEND: 'http://localhost:4200',
     JWT_SECRET: 'IMjbVViINeVwmvmj2BWg',
 }
